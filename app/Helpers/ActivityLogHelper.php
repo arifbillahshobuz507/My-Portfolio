c9c2441d034e1b298c47c5fddbb8e03df4ff5bb2
arifbillhshobuz <arifbillahmahamudaamena@gmail.com>
2026-06-14 13:33:04 -0400

    update activity log

--- app/Helpers/ActivityLogHelper.php
+++ app/Helpers/ActivityLogHelper.php
@@ -9,25 +9,21 @@ use Illuminate\Http\Request;
 
 class ActivityLogHelper
 {
-    public static function log(Request $request,  string $action, string $module, $subject = null, ?array $oldValues = null, ?array $newValues = null): void
+    public static function log(Request $request, $userId = null, string $action, string $module, $subject_id=null, $subject_type = null, ?array $oldValues = null, ?array $newValues = null): void
     {
         $user = User::where("id", $request->header('user_id'))->select('id')->first();
-        $user_id = $user ? $user->id : 0;
+        $user_id = $user ? $user->id : $userId;
         ActivityLog::create([
-            'user_id'      => auth()->id(),
-
+            'user_id'      => $user_id,
             'action'       => $action,
             'module'       => $module,
-
-            'subject_id'   => $user?->id,
-            'subject_type' => $subject ? get_class($subject) : null,
-
+            'subject_id'   => $subject_id,
+            'subject_type' => $subject_type,
             'old_values'   => $oldValues,
             'new_values'   => $newValues,
-
-            'ip_address'   => request()->ip(),
-            'user_agent'   => request()->userAgent(),
-            'url'          => request()->fullUrl(),
+            'ip_address' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'url' => $request->fullUrl(),
         ]);
     }
 }
